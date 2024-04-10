a9e83bb0e7a96c5ae9ea712f2b578d8263fb353d
JirapatJean <jirapat.nate@outlook.com>
2024-04-10 16:00:04 +0700

    feat: Automate DataProc DAG

---
+++ dag/scripts/upload_and_download_dataset.py
@@ -0,0 +1,31 @@
+from google.cloud import storage
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, destination_file_name
+        )
+    )
+
+download_blob(
+    'asia-southeast1-top-spotify-787969f2-bucket',
+    'dags/airflow_monitoring.py',
+    'monitoring.py'
+)
